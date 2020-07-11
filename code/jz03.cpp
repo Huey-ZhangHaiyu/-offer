755e65dbc267e3c9c79411fd943b33a8a96d9555
Haiyu Zhang <68046600+Huey-ZhangHaiyu@users.noreply.github.com>
2020-07-10 22:44:36 -0500

    Create jz03.cpp

---
+++ code/jz03.cpp
@@ -0,0 +1,37 @@
+//jz03 reverse a linked list
+//stack solution
+class Solution {
+public:
+    vector<int> printListFromTailToHead(ListNode* head) {
+        vector<int> value;
+        ListNode *p=NULL;
+        p=head;
+        stack<int> stk;
+        while(p!=NULL){
+            stk.push(p->val);
+            p=p->next;
+        }
+        while(!stk.empty()){
+            value.push_back(stk.top());
+            stk.pop();
+        }
+        return value;
+    }
+};
+
+//recrusion
+class Solution {
+public:
+    vector<int> value;
+    vector<int> printListFromTailToHead(ListNode* head) {
+        ListNode *p=NULL;
+        p=head;
+        if(p!=NULL){
+            if(p->next!=NULL){
+                printListFromTailToHead(p->next);
+            }
+            value.push_back(p->val);
+        }
+        return value;            
+        }
+};
