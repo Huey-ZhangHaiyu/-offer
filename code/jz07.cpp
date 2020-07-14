704359b1a70c2a037049720ecc1ebd8d64152f83
Haiyu Zhang <68046600+Huey-ZhangHaiyu@users.noreply.github.com>
2020-07-13 22:18:31 -0500

    Update jz07.cpp

--- code/jz07.cpp
+++ code/jz07.cpp
@@ -18,12 +18,18 @@ public:
 };
 
 //尾递归
+/*
+尾递归的本质，其实是将递归方法中的需要的“所有状态”通过方法的参数传入下一次调用中
+尾递归的调用处于方法的最后，因此方法之前所积累下的各种状态对于递归调用结果已经没有任何意义，
+因此完全可以把本次方法中留在堆栈中的数据完全清除，把空间让给最后的递归调用。
+这样的优化1便使得递归不会在调用堆栈上产生堆积，意味着即时是“无限”递归也不会让堆栈溢出。
+这便是尾递归的优势
+*/
 public class Solution {
     public int Fibonacci(int n) {
         return Fibonacci(n,0,1);
     }
-     
-     
+          
     private static int Fibonacci(int n,int acc1,int acc2){
         if(n==0) return 0;
         if(n==1) return acc2;
@@ -31,15 +37,3 @@ public class Solution {
          
     }
 }
-
-//动态规划，避免纯递归的溢出
-if(n<=1){
-    return n;
-}
-int[] record = new int[n+2];
-record[0] = 0;
-record[1] = 1;
-for(int i=2;i<=n;i++){
-    record[i] = record[i-1] + record[i-2];
-}
-return record[n];
