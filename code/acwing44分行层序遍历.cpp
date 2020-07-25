2c6b741fd7a07f2c077212552ade45b191af26d9
Haiyu Zhang <68046600+Huey-ZhangHaiyu@users.noreply.github.com>
2020-07-24 23:17:35 -0500

    Update acwing44分行层序遍历.cpp

--- code/acwing44分行层序遍历.cpp
+++ code/acwing44分行层序遍历.cpp
@@ -10,3 +10,33 @@
 
 输出：[[8], [12, 2], [6], [4]]
 */
+//上一题的基础上改变，队列中的每一行最后加入null标识符
+class Solution {
+public:
+    vector<vector<int>> printFromTopToBottom(TreeNode* root) {
+        vector<vector<int>> res;
+        vector<int> level;
+        if(!root) return res;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        
+        while(q.size()){
+            auto t=q.front();
+            q.pop();
+            if(t){
+                level.push_back(t->val);
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+            }
+            else if(q.size()){//加条件防止只有最后一个null的时候
+            //因为循环一开始null被pop出去了，此时数组长度为空，如果再不停的push进去null，则会死循环
+                q.push(NULL);//数组里有null，长度不为零
+                res.push_back(level);
+                level.clear();
+            }
+        }
+        return res;
+    }
+};
