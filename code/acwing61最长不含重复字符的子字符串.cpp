7909b44e00d2f9d2b8b11d510ccb4db9fd19825f
Haiyu Zhang <68046600+Huey-ZhangHaiyu@users.noreply.github.com>
2020-07-30 22:32:29 -0500

    Update acwing61最长不含重复字符的子字符串.cpp

--- code/acwing61最长不含重复字符的子字符串.cpp
+++ code/acwing61最长不含重复字符的子字符串.cpp
@@ -11,5 +11,17 @@
 因为每一次j往后走的时候，i都不可能往前走，因为上一步已经确定i到j是不重复的最大值了
 所以只要每次判断j往后走一下，新的i到j之间有没有出现重复即可
 如果没有重复，i不需要往后走；如果重复，那么i就往后移动到不重复
-
 */
+class Solution {
+public:
+    int longestSubstringWithoutDuplication(string s) {
+        unordered_map<char,int> hash;//定义对应string的int值hash表
+        int res=0;//result
+        for(int i=0,j=0;j<s.size();j++){//j一直往后找
+            hash[s[j]]++;//找到j的次数+1
+            while(hash[s[j]]>1) hash[s[i++]]--;//当出现重复，i就要往后一直找，直到没有重复
+            res=max(j-i+1,res);//记录此时最大距离
+        }
+        return res;
+    }
+};
