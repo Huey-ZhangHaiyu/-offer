8733a1ccd54e0f020987b96ce4160b1314682a50
Haiyu Zhang <68046600+Huey-ZhangHaiyu@users.noreply.github.com>
2020-08-09 09:42:36 -0500

    Update and rename jz47.cpp to jz47求1+2+...+n.cpp

--- code/jz47求1+2+...+n.cpp
+++ code/jz47求1+2+...+n.cpp
@@ -1,34 +1,21 @@
 //求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
 //这题想考察的是什么？是程序短路！
 //这才是正确答案，逻辑与有个短路特点，前面为假，后面不计算
-public int Sum_Solution(int n) {
-        int sum = n;
-        n&&((sum+=Sum_Solution(n-1))>0);
-        return sum;
-    }
-    
-//循环
 class Solution {
 public:
-    int Sum_Solution(int n) {
-        int s=0;
-        for(int i=0;i<=n;i++){
-            s+=i;
-        }
-        return s;
+    int getSum(int n) {
+        int ans=n;
+        (n>0)&&(ans+=getSum(n-1));//用逻辑起到if的作用，核心还是递归做法
+        return ans;
     }
 };
 
-//递归？
+//递归，用了if
 class Solution {
 public:
-    int Sum_Solution(int n) {
-        int s=n;
-        if(n==0) return 0;
-        if(n>0){
-            s+=Sum_Solution(n-1);
-        }
-        return s;
+    int getSum(int n) {
+        if(n<=0) return 0;
+        return getSum(n-1)+n;
     }
 };
 
