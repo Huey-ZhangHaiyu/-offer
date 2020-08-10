54591d5390ab4e03e3f9c05b7bff226e2fbfbe3e
Haiyu Zhang <68046600+Huey-ZhangHaiyu@users.noreply.github.com>
2020-08-10 15:35:27 -0500

    Update 排序.cpp

--- template/排序.cpp
+++ template/排序.cpp
@@ -3,7 +3,9 @@ void quick_sort(int q[], int l, int r)
 {
     if (l >= r) return;
 
-    int i = l - 1, j = r + 1, x = q[l + r >> 1];
+    int i = l - 1, j = r + 1, x = q[l + r >> 1];//这里一定要写成这样！
+    //如果这里写成x=l+r>>1，那么后面循环当中的mid会变化，因为i和j替换后
+    //q[x]的值就变了，无法保证此时右边全都比中点值小
     while (i < j)
     {
         do i ++ ; while (q[i] < x);
