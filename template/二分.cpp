212ad796bac2256266f12c0e28e8d6005bf6f69d
Haiyu Zhang <68046600+Huey-ZhangHaiyu@users.noreply.github.com>
2020-08-11 11:15:05 -0500

    Update 二分.cpp

--- template/二分.cpp
+++ template/二分.cpp
@@ -1,7 +1,7 @@
 //整数二分
 bool check(int x) {/* ... */} // 检查x是否满足某种性质
 
-// 区间[l, r]被划分成[l, mid]和[mid + 1, r]时使用：
+// 区间[l, r]被划分成[l, mid]和[mid + 1, r]时使用：找左边界
 int bsearch_1(int l, int r)
 {
     while (l < r)
@@ -12,8 +12,8 @@ int bsearch_1(int l, int r)
     }
     return l;
 }
-// 区间[l, r]被划分成[l, mid - 1]和[mid, r]时使用：
-int bsearch_2(int l, int r)
+// 区间[l, r]被划分成[l, mid - 1]和[mid, r]时使用：找右边界
+int bsearch_2(int l, int r)//通常是(0,n-1)
 {
     while (l < r)
     {
