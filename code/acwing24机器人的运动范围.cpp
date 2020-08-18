ec7dae7349fa916afeb10eaca8b145e0e12ec44c
Haiyu Zhang <68046600+Huey-ZhangHaiyu@users.noreply.github.com>
2020-08-18 16:47:50 -0500

    Update acwing24机器人的运动范围.cpp

--- code/acwing24机器人的运动范围.cpp
+++ code/acwing24机器人的运动范围.cpp
@@ -1,10 +1,7 @@
 /*
 地上有一个 m 行和 n 列的方格，横纵坐标范围分别是 0∼m−1 和 0∼n−1。
-
 一个机器人从坐标0,0的格子开始移动，每一次只能向左，右，上，下四个方向移动一格。
-
 但是不能进入行坐标和列坐标的数位之和大于 k 的格子。
-
 请问该机器人能够达到多少个格子？
 */
 
@@ -17,7 +14,7 @@ public:
         return dfs(threshold, 0, 0, vis);
     }
     int dfs(int th, int i, int j, vector<vector<bool>> &vis){//参数传递的是形参，因此要在vis前面加个引用，表示这个数组的值已经修改
-        if(i < 0 || i >=vis.size() || j < 0 || j >= vis[i].size() || vis[i][j] ||(i/10+i%10+j/10+j%10)>th) return 0;
+        if(i < 0 || i >=vis.size() || j < 0 || j >= vis[i].size() || vis[i][j] ||(i/10+i%10+j/10+j%10)>th) return 0;//这里应该也要像下面那样写一个计算sum的函数才行
         vis[i][j]=true;
         return dfs(th,i+1, j, vis)+dfs(th,i-1, j, vis)+dfs(th,i, j-1, vis)+dfs(th,i, j+1, vis) + 1;
     }
