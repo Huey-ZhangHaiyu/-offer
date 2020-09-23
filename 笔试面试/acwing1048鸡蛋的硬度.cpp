51a164e58d395a713451feeba2ae97beeaaa4c78
Haiyu Zhang <68046600+Huey-ZhangHaiyu@users.noreply.github.com>
2020-09-23 12:55:20 -0500

    Create acwing1048鸡蛋的硬度.cpp

---
+++ 笔试面试/acwing1048鸡蛋的硬度.cpp
@@ -0,0 +1,21 @@
+//f[i, j]表示用j个鸡蛋测量i次能测量的区间长度的最大值
+//枚举扔鸡蛋的楼层k，类似dp1，没碎测k楼以上，碎了测k楼以下，那么能测的最大高度就是上下两部分加上第k层楼这一层
+
+#include <iostream>
+using namespace std;
+
+int f[110][15], n, m;
+
+int main() {
+    while (cin >> n >> m) {
+        for (int i = 1; i <= n; i ++ ) {
+            for (int j = 1; j <= m; j ++ )
+                f[i][j] = f[i - 1][j] + f[i - 1][j - 1] + 1;
+            if (f[i][m] >= n) {
+                cout << i << endl;
+                break;
+            }
+        }
+    }
+    return 0;
+}
